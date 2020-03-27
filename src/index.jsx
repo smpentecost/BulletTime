2fb3a8849f249c3666578534662d543ba9d95a74
Seeley <smpentecost@gmail.com>
2020-03-27 09:22:26 -0400

    switching to incremental auto space

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -36,11 +36,17 @@ function GuidedBulletArea(props) {
 
 function BulletTester(props) {
   const ref = useRef(null);
-  
+  const [width, setWidth] = useState(0);
+
+  //Set the local state width any time the screen renders
   useEffect(() => {
-    const width = ref.current ? ref.current.offsetWidth : 0;
-    console.log('width', width);
+    setWidth(ref.current ? ref.current.offsetWidth : 0);
   });
+
+  //Set the parent width any time the local state width changes
+  useEffect(() => {
+    props.handleWidthChange(props.index, width);
+  }, [width]);
   
   return (
     <div ref={ref} className="bullet-tester">
@@ -69,11 +75,82 @@ class BulletEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bullets: Array(1).fill(null), //'- Develops threat radar models/simulations; drives US/Allied radar warning reprogramming & intel mission data feeds',
+      bullets: [], //'- Develops threat radar models/simulations; drives US/Allied radar warning reprogramming & intel mission data feeds',
+      widths: [],
       graberized: false,
       guides: [{left: '600px'}, {left: '763px'}, {left: '800px'}],
       test_string: null,
     };
+  }
+
+
+
+  autoSpace(bullet, index) {
+    /*
+      The best way to do this is incrementally, checking length as we
+      go. That is because these spaces don't have specific whitespace
+      pixels associated with them. Instead their pixel spaces are
+      dynamic based on font style and size. So while it sucks that we
+      need to use a timeout delay, atleast it only happens when the
+      user toggles the spacing on.
+
+      The whitespace unicode available for use with an OPR:
+      2006:approx-2px(smallest), 2009:approx-1px(small),
+      2004:approx+2px(big), 2007:approx+4px(biggest)
+    */
+
+    const isWhiteSpace = (element) => {
+      switch(element) {
+      case "\u2004": return true;
+      case "\u2006": return true;
+      case "\u2007": return true;
+      case "\u2009": return true;
+      case " ": return true;
+      default: return false;
+      }
+    };
+
+    let spaces = [];
+    let bulletArray = bullet.split("");
+    let difference = 762 - this.state.widths[index];
+    
+    let position = bulletArray.findIndex(isWhiteSpace);
+    while (position !== -1) {
+      spaces.push(position);
+      let subArrayPosition = bulletArray.slice(position + 1).findIndex(isWhiteSpace);
+      if (subArrayPosition > 0) {
+        position = subArrayPosition + position + 1;
+      } else {
+        position = subArrayPosition;
+      }
+    }
+    //console.log(spaces);
+    //console.log(bulletArray);
+
+    /*
+    if (difference > 0) {
+      let big_replacements = Math.floor(difference/2);
+      let biggest_replacements = big_replacements-spaces.length;
+      for (let i = 0; i < spaces.length; i++) {
+        if (i <= biggest_replacements) {
+          bulletArray[spaces[i]] = "\u2007";
+        } else if (i <= big_replacements) {
+          bulletArray[spaces[i]] = "\u2004";
+        }
+      }
+    } else {
+      let small_replacements = (Math.floor(difference/1)*-1) + 1;
+      let smallest_replacements = small_replacements-spaces.length;
+      for (let i = 0; i < spaces.length; i++) {
+        if (i <= smallest_replacements) {
+          bulletArray[spaces[i]] = "\u2006";
+        } else if (i <= small_replacements) {
+          bulletArray[spaces[i]] = "\u2009";
+        }
+      }
+
+    }*/
+    
   }
 
   handleBulletChange(event) {
@@ -87,17 +164,23 @@ class BulletEditor extends React.Component {
 
     bullets.forEach(function(bullet, index) {
       if (!this.state.graberized) {
-        bullets[index] = bullet.replace(/ /g, "\u200A");
+        this.autoSpace(bullet, index);
       } else {
         bullets[index] = bullet.replace(
-          /[\u2002\u2003\u2004\u2006\u2007\u2009\u200A]/g,
-          " "
+          /[\u2004\u2006\u2007\u2009]/g,
+          ' '
         );
       }
     }, this);
     this.setState({bullets: bullets,
                    graberized: !this.state.graberized
                   });
+  }
+
+  handleWidthChange(index, width) {
+    let widths = this.state.widths;
+    widths[index] = width;
+    this.setState({widths: widths});
   }
 
   createBulletTesters() {
@@ -108,9 +191,11 @@ class BulletEditor extends React.Component {
       testers.push(
         <BulletTester
           value={bullet}
+          index={index}
+          handleWidthChange={(index, width) => this.handleWidthChange(index, width)}
         />
       );
-    });
+    }, this);
 
     return(testers);
   }
@@ -124,7 +209,6 @@ class BulletEditor extends React.Component {
           checked={this.state.graberized}
           onClick={() => this.handleAutoSpace()}
         />
-        <br/>
         <GuidedBulletArea
           value={this.state.bullets.join("\n")}
           guides={this.state.guides}
