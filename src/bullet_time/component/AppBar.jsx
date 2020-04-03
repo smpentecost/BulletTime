b3e346e24d3c4800d6e609e79ffd472afc4cabf7
Seeley <smpentecost@gmail.com>
2020-04-03 11:08:13 -0400

    restructured directory

--- src/bullet_time/component/AppBar.jsx
+++ src/bullet_time/component/AppBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import HeaderImage from '../../images/header.jpg';
+import HeaderImage from '../images/header.jpg';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
